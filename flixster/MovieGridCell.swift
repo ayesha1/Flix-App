ce2b739f5f780a9cc1fb8cfdc9d77b46d5df2f1d
Ayesha Khan <akhan1231@hotmail.com>
2019-02-10 20:30:43 -0800

    updated part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Ayesha Khan on 2/10/19.
+//  Copyright © 2019 Ayesha Khan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
